32c2414e60b7f85cdd07d228ab9ee95000970aba
Jose Rodriguez <jose@MacBook-Air-de-Jose.local>
2025-03-21 15:27:56 +0100

    Update backend with logs

--- server.js
+++ server.js
@@ -3,23 +3,16 @@ const twilio = require('twilio');
 const cors = require('cors');
 const app = express();
 
-// Configura CORS para permitir solicitudes desde tu dominio de Netlify
-app.use(cors({
-  origin: 'https://curaesencial.netlify.app', // Reemplaza con tu dominio exacto de Netlify
-  methods: ['GET', 'POST'], // Métodos permitidos
-  allowedHeaders: ['Content-Type'] // Encabezados permitidos
-}));
-
 app.use(express.json());
+app.use(cors({ origin: 'https://curaesencial.netlify.app' }));
 
-// Configuración de Twilio usando variables de entorno
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = new twilio(accountSid, authToken);
 
 app.post('/send-sms', async (req, res) => {
   const { to, message } = req.body;
-  console.log('Solicitud recibida:', { to, message }); // Añade este log
+  console.log('Solicitud recibida:', { to, message });
 
   try {
     await client.messages.create({
@@ -27,10 +20,10 @@ app.post('/send-sms', async (req, res) => {
       from: process.env.TWILIO_PHONE_NUMBER,
       to: to
     });
-    console.log('SMS enviado con éxito a:', to); // Añade este log
+    console.log('SMS enviado con éxito a:', to);
     res.status(200).send('SMS enviado');
   } catch (error) {
-    console.error('Error al enviar SMS:', error); // Añade este log
+    console.error('Error al enviar SMS:', error);
     res.status(500).send('Error al enviar SMS');
   }
 });
